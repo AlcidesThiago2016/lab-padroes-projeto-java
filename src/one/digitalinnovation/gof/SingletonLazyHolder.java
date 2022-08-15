75f51f017270396b05aca3b2752d337ae4b6db51
AlcidesThiago2016 <alcides_thiago@hotmail.com>
2022-08-15 11:57:01 -0300

    Initial commit

---
+++ src/one/digitalinnovation/gof/SingletonLazyHolder.java
@@ -0,0 +1,20 @@
+package one.digitalinnovation.gof;
+
+/**
+ * Singleton "Lazy Holder"
+ * @author AlcidesThiago2016
+ */
+public class SingletonLazyHolder {
+
+    private static class InstanceHolder{
+      public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+    private SingletonLazyHolder(){
+        super();
+    }
+
+    public static SingletonLazyHolder getInstancia() {
+        return InstanceHolder.instancia;
+    }
+}
